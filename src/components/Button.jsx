31edd3e5773da05c2e0e46aaac0b5ec59e5b6431
simondelasheras <simidlh@gmail.com>
2023-10-02 21:20:35 -0300

    agregamos el boton de carrito

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,11 @@
 'use client'
+import React from 'react';
 
 function Button() {
   return (
     <div>
         
-    <button className="btn-container" onClick={()=> alert ('Agregado al carrito!')}>Comprar</button>
+    <button className="btn-container" onClick={()=> alert ('Agregado al carrito!')}>Agregar al carrito</button>
 
     </div>
   )
